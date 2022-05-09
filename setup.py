9705ff30da00cd98232ca1ffdf9a7d675ad6437c
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-09 18:19:46 +0900

    Update setup.py for pypi description

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
 
 setup(
     name='pyqt-bounding-box',
@@ -9,6 +18,8 @@ setup(
     packages=find_packages(),
     description='PyQt bounding box for graphic design software',
     url='https://github.com/yjg30737/pyqt-bounding-box.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
